8d091eb0a1962beebb18f1bdc31820c1315ccab1
AntwonTheDamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-04-03 17:03:39 -0400

    Begin writing ship constructor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import ship from './modules/ship.js'
 import './style/style.css';
+
+const sh1 = new ship.ShipConstructor(2);
+
+sh1.hit();
---
+++ src/modules/ship.js
@@ -0,0 +1,17 @@
+const ship = (() => {
+
+    const ShipConstructor = (health) => {
+        this.health = health;
+    }
+    
+    // ShipConstructor.prototype.hit = function() {
+    //     console.log(this.health);
+    // }
+
+    // const generateShips = () => {
+    
+    // }
+    return ShipConstructor
+})();
+
+export default ship
